e93ffd9c77b2229d80d55084d5191d8ef564c7d5
lucascco <lucasccorrea@gmail.com>
2017-06-13 16:37:57 -0300

    first commit

---
+++ src/pagination.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pluritech-pagination',
+  template: `<h1>Here is pagination</h1>`
+})
+export class PaginationComponent {
+
+  constructor() {
+  }
+
+}
