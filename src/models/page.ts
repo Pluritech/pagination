e37b1c6350070c0207e5ad038347b9ab3ea4c9ca
lucascco <lucasccorrea@gmail.com>
2017-06-14 10:37:29 -0300

    Início da implementação

---
+++ src/models/page.ts
@@ -0,0 +1,6 @@
+export class Page {
+  nPage: number;
+  total: number;
+  offset: number;
+  limit: number;
+}
--- src/pagination.component.ts
+++ src/pagination.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, EventEmitter, Output } from '@angular/core';
+
+import { Page } from './models/page';
 
 @Component({
   selector: 'pluritech-pagination',
-  template: `<h1>Here is pagination</h1>`
+  template: `
+    <h1>Here is pagination</h1>
+    {{total}}
+  `
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
+
+  private firstPage: number;
+  private lastPage: number;
+  private pageActive: number = 1;
+  private totPages: number;
+  private arrayPages: number[] = [];
+
+  @Input() total: number = 0;
+  @Input() limit: number = 1;
+
+  @Output() changePage: EventEmitter<Page> = new EventEmitter<Page>();
 
   constructor() {
+  }
+
+  public next() {
+    if (this.canNext()) {
+      this.goPage(++this.pageActive);
+    }
+  }
+
+  public prev() {
+    if (this.canPrev()) {
+      this.goPage(--this.pageActive);
+    }
+  }
+
+  public goPage(nPage: number) {
+    const offset = this.limit * (nPage - 1);
+    this.changePage.emit({
+      limit: this.limit,
+      nPage: nPage,
+      offset: offset,
+      total: this.total
+    });
+  }
+
+  public showPagination(): boolean {
+    return this.total > 0;
+  }
+
+  public canPrev(): boolean {
+    return this.pageActive > this.firstPage;
+  }
+
+  public canNext(): boolean {
+    return this.pageActive < this.lastPage;
+  }
+
+  ngOnChanges() {
+    if (this.showPagination()) {
+      // init the pagination
+      this.totPages = this.lastPage = Math.ceil((this.total / this.limit));
+    }
   }
 
 }
